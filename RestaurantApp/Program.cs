ef9a0594e6bbeaf707297e3dd17b93c387163f67
Samat Murat <muratsamat090598@gmail.com>
2020-11-05 23:01:18 +0600

    Реализовал логику формирование html файла с даныыми о столиках в виде таблицы

---
+++ RestaurantApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace RestaurantApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var datasource = @"HOME-PC\SQLEXPRESS";
+            var database = "Restaurant";
+
+             
+            string connString = @"Data Source=" + datasource + ";Initial Catalog="
+                                + database + ";Integrated Security=True;";
+
+            
+            SqlConnection conn = new SqlConnection(connString);
+
+            FileManager fileManager = new FileManager(conn);
+            fileManager.CreateHtmlFile();
+        }
+    }
+}
---
+++ RestaurantApp/Table.cs
@@ -0,0 +1,10 @@
+namespace RestaurantApp
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public int Size { get; set; }
+        public int FreeSize { get; set; }
+        public bool IsFree => FreeSize == Size;
+    }
+}
